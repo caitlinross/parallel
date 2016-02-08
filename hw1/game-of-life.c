031b094263b5b89d57595cdac20a47133785f183
Caitlin Ross <rossc3@rpi.edu>
2016-02-08 12:16:26 -0500

    added error checking, comments, and file output

--- hw1/game-of-life.c
+++ hw1/game-of-life.c
@@ -60,14 +60,36 @@ int main(int argc, char *argv[])
     // Convert strings to integer values using "atoi()"
     if (argc == 4)
     {
-        //TODO add in error checking
-        g_x_cell_size = atoi(argv[1]);
-        g_y_cell_size = atoi(argv[1]);
-        g_num_ticks = atoi(argv[2]);
-        g_threshold = atoi(argv[3])*.01;
+        if (atoi(argv[1]) > 0)
+        {
+            g_x_cell_size = atoi(argv[1]);
+            g_y_cell_size = atoi(argv[1]);
+        }
+        else
+        {
+            perror("Grid size should be an integer larger than 0");
+            exit(1);
+        }
+        if (atoi(argv[2]) > 0)
+            g_num_ticks = atoi(argv[2]);
+        else
+        {
+            perror("Number of ticks should be an integer larger than 0");
+            exit(1);
+        }
+        if (atoi(argv[3]) >= 0)
+            g_threshold = atoi(argv[3])*.01;
+        else
+        {
+            perror("Threshold should be an integer greater than or equal to 0");
+            exit(1);
+        }
     }
     else
+    {
         perror("Expects 3 arguments! size, num_ticks, and threshold");
+        exit(1);
+    }
 
     allocate_and_init_cells();
 
@@ -91,10 +113,21 @@ void allocate_and_init_cells()
     // use "calloc" to allocate space for your cell matrix
     // use "drand48" to init the state of each grid cell once allocated.
     g_GOL_CELL = calloc(g_x_cell_size, sizeof(unsigned int*));
+    if (!g_GOL_CELL)
+    {
+        perror("calloc failed");
+        exit(1);
+    }
+
     for (i = 0; i < g_x_cell_size; i++)
     {
-        //TODO calloc error checking
         g_GOL_CELL[i] = calloc(g_y_cell_size, sizeof(unsigned int));
+        if (!g_GOL_CELL[i])
+        {
+            perror("calloc failed");
+            exit(1);
+        }
+
         for (j = 0; j < g_y_cell_size; j++)
         {
             double r = drand48();
@@ -138,26 +171,41 @@ void compute_one_tick()
 
 void output_final_cell_state()
 {
-    // TODO send output to file
     // print out in grid form 16 value per row of the g_GOL_CELL
     // This data will be used to create your graphs
+    FILE *f;
+    char filename[64];
+    sprintf(filename, "gol-%d-%d-%d.out", g_x_cell_size, g_num_ticks, (int)(g_threshold*100));
+    f = fopen(filename, "w");
+    if (!f)
+    {
+        perror("unable to open file");
+        exit(1);
+    }
+
     int i, j;
     for (i = 0; i < g_x_cell_size; i++)
     {
+        char line[g_x_cell_size * 2 + 1];
+        sprintf(line, "");
         for (j = 0; j < g_y_cell_size; j++)
         {
-            if (j == g_y_cell_size - 1)
-                printf("%d\n", g_GOL_CELL[i][j]);
-            else
-                printf("%d,", g_GOL_CELL[i][j]);
+            char tmp[4];
+            sprintf(tmp, "%d ", g_GOL_CELL[i][j]);
+            strcat(line, tmp);
+
         }
+        fprintf(f, "%s\n", line);
+        printf("%s\n", line);
     }
+    fclose(f);
 }
 
 /***************************************************************************/
 /* Helper Functions ********************************************************/
 /***************************************************************************/
 
+/* apply the game of life rules to the cell in position (i,j) */
 void apply_gol_rules(int i, int j)
 {
     int total_alive = count_alive_neighbors(i, j);
@@ -175,14 +223,15 @@ void apply_gol_rules(int i, int j)
     }
 }
 
+/* count alive neighbors */
 int count_alive_neighbors(int i, int j)
 {
-    // count alive neighbors
     int total_alive = 0;
     int i_u = mod(i+1, (int)g_x_cell_size);
     int i_d = mod(i-1, (int)g_x_cell_size);
     int j_r = mod(j+1, (int)g_y_cell_size);
     int j_l = mod(j-1, (int)g_y_cell_size);
+    // up, down, left, right neighbors
     if (g_GOL_CELL[i_u][j] == ALIVE)
         total_alive++;
     if (g_GOL_CELL[i_d][j] == ALIVE)
@@ -205,10 +254,13 @@ int count_alive_neighbors(int i, int j)
     return total_alive;
 }
 
+/* % doesn't work the way I want it to when a < 0
+ * so use this mod function instead
+ */
 int mod (int a, int b)
 {
-    int ret = a % b;
-    if(ret < 0)
-        ret+=b;
-    return ret;
+    int rem = a % b;
+    if(rem < 0)
+        rem += b;
+    return rem;
 }
