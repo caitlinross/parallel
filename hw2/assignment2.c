6b5040d3f342112ddfdb2e186431f37aacd237cd
Caitlin Ross <rossc3@rpi.edu>
2016-02-14 00:25:16 -0500

    made some changes to get working with MPI

--- hw2/assignment2.c
+++ hw2/assignment2.c
@@ -35,14 +35,17 @@ unsigned int g_y_cell_size=0;
 unsigned int g_num_ticks=0;
 double g_threshold=0.0;
 
+unsigned int **ghost_row_before = NULL;
+unsigned int **ghost_row_after = NULL;
+
 /***************************************************************************/
 /* Function Decs ***********************************************************/
 /***************************************************************************/
 
 // Bring over from your code
-void allocate_and_init_cells();
-void compute_one_tick();
-void output_final_cell_state();
+void allocate_and_init_cells(int num_rows, int rank);
+void compute_one_tick(int num_rows, int rank);
+void output_final_cell_state(int num_rows, int rank);
 void apply_gol_rules(int i, int j);
 int count_alive_neighbors(int i, int j);
 int mod (int a, int b);
@@ -54,13 +57,16 @@ int mod (int a, int b);
 
 int main(int argc, char *argv[])
 {
-//    int i = 0;
+    int i = 0;
     int mpi_myrank;
     int mpi_commsize;
+    double start_time, end_time;
+
 // Example MPI startup and using CLCG4 RNG
     MPI_Init( &argc, &argv);
     MPI_Comm_size( MPI_COMM_WORLD, &mpi_commsize);
     MPI_Comm_rank( MPI_COMM_WORLD, &mpi_myrank);
+    start_time = MPI_Wtime();
     
 // Init 16,384 RNG streams - each rank has an independent stream
     InitDefault();
@@ -71,10 +77,55 @@ int main(int argc, char *argv[])
     
     MPI_Barrier( MPI_COMM_WORLD );
 // Bring over rest from your code
-    
+    if (argc == 4)
+    {
+        if (atoi(argv[1]) > 0)
+        {
+            g_x_cell_size = atoi(argv[1]);
+            g_y_cell_size = atoi(argv[1]);
+        }
+        else
+        {
+            perror("Grid size should be an integer larger than 0");
+            exit(1);
+        }
+        if (atoi(argv[2]) > 0)
+            g_num_ticks = atoi(argv[2]);
+        else
+        {
+            perror("Number of ticks should be an integer larger than 0");
+            exit(1);
+        }
+        if (atoi(argv[3]) >= 0)
+            g_threshold = atoi(argv[3])*.01;
+        else
+        {
+            perror("Threshold should be an integer greater than or equal to 0");
+            exit(1);
+        }
+    }
+    else
+    {
+        perror("Expects 3 arguments! size, num_ticks, and threshold");
+        exit(1);
+    }
+
+
+
+    // TODO need to make sure it works correctly when num rows not evenly divisible
+    allocate_and_init_cells(g_y_cell_size / mpi_commsize, mpi_myrank);
+
+    for(i = 0; i < g_num_ticks; i++)
+    {
+        compute_one_tick(g_y_cell_size / mpi_commsize, mpi_myrank);
+    }
+
+    output_final_cell_state(g_y_cell_size / mpi_commsize, mpi_myrank);
 
 // END -Perform a barrier and then leave MPI
     MPI_Barrier( MPI_COMM_WORLD );
+    end_time = MPI_Wtime();
+    printf("Run time: %f seconds\n", end_time-start_time);
     MPI_Finalize();
     return 0;
 }
@@ -86,60 +137,79 @@ int main(int argc, char *argv[])
 /* Function: allocate_and_init_cells ***************************************/
 /***************************************************************************/
 
-void allocate_and_init_cells()
+void allocate_and_init_cells(int num_rows, int rank)
 {
+    // initialize my own rows of universe
     int i, j;
-    // use "calloc" to allocate space for your cell matrix
-    // use "drand48" to init the state of each grid cell once allocated.
-    g_GOL_CELL = calloc(g_x_cell_size, sizeof(unsigned int*));
+    g_GOL_CELL = calloc(num_rows, sizeof(unsigned int*));
     if (!g_GOL_CELL)
     {
         perror("calloc failed");
+        MPI_Finalize();
         exit(1);
     }
 
-    for (i = 0; i < g_x_cell_size; i++)
+    for (i = 0; i < num_rows; i++)
     {
-        g_GOL_CELL[i] = calloc(g_y_cell_size, sizeof(unsigned int));
+        g_GOL_CELL[i] = calloc(g_x_cell_size, sizeof(unsigned int));
         if (!g_GOL_CELL[i])
         {
             perror("calloc failed");
+            MPI_Finalize();
             exit(1);
         }
 
-        for (j = 0; j < g_y_cell_size; j++)
+        for (j = 0; j < g_x_cell_size; j++)
         {
-            double r = drand48();
+            // TODO check that this is correct generator number
+            double r = GenVal(rank * num_rows + i);
             if (r < .5)
                 g_GOL_CELL[i][j] = ALIVE;
             else
                 g_GOL_CELL[i][j] = DEAD;
         }
     }
+
+    // allocate memory for ghost rows
+    ghost_row_before = calloc(g_x_cell_size, sizeof(unsigned int));
+    if (!ghost_row_before)
+    {
+        perror("calloc failed");
+        MPI_Finalize();
+        exit(1);
+    }
+    ghost_row_after = calloc(g_x_cell_size, sizeof(unsigned int));
+    if (!ghost_row_after)
+    {
+        perror("calloc failed");
+        MPI_Finalize();
+        exit(1);
+    }
+    
+
 }
 
 /***************************************************************************/
 /* Function: compute_one_tick **********************************************/
 /***************************************************************************/
 
-void compute_one_tick()
+void compute_one_tick(int num_rows, int rank)
 {
-    // iterate over X (outside loop) and Y (inside loop) dimensions of the g_GOL_CELL
-    // Use drand48() for uniform distribution. It is already included in stdlib.h
+    // TODO need to add in mpi messaging for ghost rows 
     int i, j;
-    for (i = 0; i < g_x_cell_size; i++)
+    for (i = 0; i < num_rows; i++)
     {
-        for (j = 0; j < g_y_cell_size; j++)
+        for (j = 0; j < g_x_cell_size; j++)
         {
-            double r = drand48();
-            if (r > g_thresh_hold)
+            double r = GenVal(rank * num_rows + i);
+            if (r > g_threshold)
             {
                 // random number greater than given threshold
                 // use normal GOL rules
                 apply_gol_rules(i, j);
             }
             else // otherwise choose a random state
-                g_GOL_CELL[i][j] = (drand48() < .5) ? ALIVE : DEAD;
+                g_GOL_CELL[i][j] = (GenVal(rank * num_rows + i) < .5) ? ALIVE : DEAD;
         }
     }
 }
@@ -148,26 +218,27 @@ void compute_one_tick()
 /* Function: output_final_cell_state ***************************************/
 /***************************************************************************/
 
-void output_final_cell_state()
+void output_final_cell_state(int num_rows, int rank)
 {
     // print out in grid form 16 value per row of the g_GOL_CELL
     // This data will be used to create your graphs
     FILE *f;
     char filename[64];
-    sprintf(filename, "gol-%d-%d-%d.out", g_x_cell_size, g_num_ticks, (int)(g_thresh_hold*100));
+    sprintf(filename, "gol-%d-%d-%d-rank%d.out", g_x_cell_size, g_num_ticks, (int)(g_threshold*100), rank);
     f = fopen(filename, "w");
     if (!f)
     {
         perror("unable to open file");
+        MPI_Finalize();
         exit(1);
     }
 
     int i, j;
-    for (i = 0; i < g_x_cell_size; i++)
+    for (i = 0; i < num_rows; i++)
     {
         char line[g_x_cell_size * 2 + 1];
         line[0]=0;
-        for (j = 0; j < g_y_cell_size; j++)
+        for (j = 0; j < g_x_cell_size; j++)
         {
             char tmp[4];
             sprintf(tmp, "%d ", g_GOL_CELL[i][j]);
@@ -205,6 +276,7 @@ void apply_gol_rules(int i, int j)
 /* count alive neighbors */
 int count_alive_neighbors(int i, int j)
 {
+    // TODO have to change to account for ghost rows
     int total_alive = 0;
     int i_u = mod(i+1, (int)g_x_cell_size);
     int i_d = mod(i-1, (int)g_x_cell_size);
